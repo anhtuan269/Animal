05a3f14e7a90331c05bb8a5d17de9c4abba41059
Anh_tuan <anhtuan0907@gmail.com>
2020-08-04 23:40:36 +0700

    add Dog file and add prototype for Cat

--- mouse.js
+++
@@ -1,9 +0,0 @@
-function Mouse(name,age){
-    this.name = name;
-    this.age = age;
-    this.dead = false;
-}
-Mouse.prototype.die = function(){
-    this.dead = true;
-}
-module.exporst = Mouse;
